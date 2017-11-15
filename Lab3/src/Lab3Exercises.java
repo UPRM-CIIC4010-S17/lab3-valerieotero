9bb53d302f2b7b4d660408f17111c30559206913
4015a13 <4015a13@mango.ece.uprm.edu>
2017-11-15 12:24:18 -0400

    changed: tittle; location and size

--- Lab3/src/Lab3Exercises.java
+++ Lab3/src/Lab3Exercises.java
@@ -4,7 +4,7 @@ public class Lab3Exercises {
 	public static void main(String[] args) {
 		JFrame myFrame = new JFrame("Valerie Otero");
 		myFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-		myFrame.setLocation(1000, 400);
+		myFrame.setLocation(1000, 300);
 		myFrame.setSize(200, 200);
 		myFrame.setVisible(true);
 	}
